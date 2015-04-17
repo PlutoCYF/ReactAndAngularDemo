c75a2578bfb21a3cbded856a15f2dd70e0da0de1
PlutoCYF <cclander@126.com>
2015-04-17 11:17:11 +0800

    一些研究例子

---
+++ Script/demo4.js
@@ -0,0 +1,10 @@
+﻿angular.module('initExample', [])
+
+            .controller('MyCtrl', ['$scope', function MyCtrl($scope) {
+                $scope.action = function () {
+                    $scope.name = 'OK';
+                    //setTimeout(function () {  }, 0);
+                }
+
+                //$scope.name = 'World';
+            }]);
---
+++ pages/react/app.js
@@ -0,0 +1,19 @@
+﻿var CarList = React.createClass({
+    render: function() {
+        var createItem = function(item) {
+            return  (
+                <div>
+                车名：{item.name},车牌号：{item.num}
+                </div>
+            );
+        };
+        return (<div>{this.props.Cars.map(createItem)}</div>);
+    }
+});
+var cars = [
+    {name:"汽车1",num:"123123"},
+    {name:"汽车1",num:"123123"},
+    {name:"汽车1",num:"123123"},
+    {name:"汽车1",num:"123123"}
+];
+React.render(<CarList Cars={cars} />, document.getElementById("list"));
